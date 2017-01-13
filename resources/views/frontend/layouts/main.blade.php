fc30104ff0fc76b2d29642f07fcebc8868da7650
unknown <matej@irectestcroliday.info>
2017-01-13 12:12:44 +0100

    test push all work

--- resources/views/frontend/layouts/main.blade.php
+++ resources/views/frontend/layouts/main.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <title>Euroreklam Klime</title>
-
+  <link rel="stylesheet" type="text/css" href="css/frontend/app.css">
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
